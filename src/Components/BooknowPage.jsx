85a115f2b2fc9f0864b64f1947ce6fbbdd346b55
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-10 19:35:38 +0530

    Add files via upload

--- src/Components/BooknowPage.jsx
+++ src/Components/BooknowPage.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const BooknowPage = () => {
 
+    const [fname, setFname] = useState('')
+    const [email, setEmail] = useState('')
+    const [cname, setCname] = useState('')
+    const [title, setTitle] = useState('')
+
+    const handleFName = (e) => {
+        setFname(e.target.value)
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleCompanyName = (e) => {
+        setCname(e.target.value)
+    }
+
+    const handleTitle = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        const LocalArray = localStorage.getItem('LocalArray') && localStorage.getItem('LocalArray').length > 0 ? JSON.parse(localStorage.getItem('LocalArray')) : []
+        localStorage.setItem('LocalArray', JSON.stringify([...LocalArray, {fname, email, cname, title}])) 
+        alert("Regiter successful datastore")
+        window.location.reload()
+    }
+
     return (
         <div className='container'>
-            <div>
+            <div className='subcontainerf'>
                 <img className='FImg'  src='https://voosh.in/static/media/landing_bg.da8497cd24d321282c0d.webp' />
                 <img className='headerImg' src='https://voosh.in/static/media/VooshLogo.c64bcebd40a2d49cc591.webp' />   
                 <img className='SImg' src='https://voosh.in/static/media/my_profile.61da110e7e1e80c97356.webp' />
-                {/* <img className='amazonImg' src='https://voosh.in/static/media/page1_2.4bfc2d124bbd2e50da2d.webp' /> */}
+                <img className='arrowImg' src='https://voosh.in/static/media/three_lines.cb3005e94acb8877cb80.webp' />
+                <img className='grumbleImg' src='https://voosh.in/static/media/page1_6.2213ad7f35bd24758114.webp' />
+                <img className='eatImg' src='https://voosh.in/static/media/page1_4.a2a22e7f5e42a50552ae.webp' />
+                <img className='tirImg' src='https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp' />
+                <img className='amazonImg' src='https://voosh.in/static/media/page1_2.4bfc2d124bbd2e50da2d.webp' />
+                <img className='swiggyImg' src='https://voosh.in/static/media/page1_5.f505a37d3e86d1a571da.webp' />
+
                 <a href='#subcontainer'><button className='btn'>Book Demo</button></a>
             </div>
                 
@@ -18,14 +52,14 @@ const BooknowPage = () => {
                         <div className='book'>Book A Demo Now</div>
                         <div className='bookdemoDiv'>
                             <label>Name</label>
-                            <input type="text" name='name'  placeholder='Ex John Doe'  />
+                            <input type="text" name='name'  placeholder='Ex John Doe' onChange={(e)=> handleFName(e)} value={fname} />
                             <label>Email</label>
-                            <input type="email" name='email'  placeholder='Ex mail@website.com'  />
+                            <input type="email" name='email'  placeholder='Ex mail@website.com' onChange={(e)=> handleEmail(e)} value={email}  />
                             <label>Company Name</label>
-                            <input type="text" name='cname'  placeholder='Ex XYZ PVT LTD'  />
+                            <input type="text" name='cname'  placeholder='Ex XYZ PVT LTD' onChange={(e)=> handleCompanyName(e)} value={cname} />
                             <label>Your title</label>
-                            <input type="text" name='title'  placeholder='Ex Opertational head'  />
-                            <div className='bookdemobtn' >Book Demo</div>
+                            <input type="text" name='title'  placeholder='Ex Opertational head' onChange={(e)=> handleTitle(e)} value={title}  />
+                            <div className='bookdemobtn' onClick={handleSubmit} >Book Demo</div>
                         </div>
                    </div>
             </div>
